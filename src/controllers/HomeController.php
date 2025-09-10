9736c022a4c89f03586a51b84c3714acb80b5c49
James Dolan <jamesdolan16@gmail.com>
2025-09-10 13:57:20 +0100

    Added lots of helpful stuff, the foundation of Radical, a bold (and weightless) framework

---
+++ src/controllers/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+class HomeController
+{
+    public function index()
+    {
+        echo "Hello from HomeController index method!";
+    }
+
+    public function about()
+    {
+        echo "This is the about page.";
+    }
+}
--- src/public/index.php
+++ src/public/index.php
@@ -1,3 +1,17 @@
 <?php
+require __DIR__ . '/../../vendor/autoload.php';
 
-phpinfo();
+$router = new \Bramus\Router\Router();
+
+$router->get('/', 'App\Controllers\HomeController@index');
+$router->get('/about', 'App\Controllers\HomeController@about');
+$router->get('/greet/(\w+)', function($name) {
+    echo "Hello, $name!";
+});
+
+$router->set404(function() {
+    header('HTTP/1.1 404 Not Found');
+    echo '404 - Page not found';
+});
+
+$router->run();
